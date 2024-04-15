f605f25cc057703752c58d318202e0af31c1c24d
rizki <rizkiadnanfutuh@gmail.com>
2024-04-16 01:20:09 +0700

    Add Font Color in Shared Variables

--- travelyuk_app/lib/shared/theme.dart
+++ travelyuk_app/lib/shared/theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Color kPrimaryColor = const Color(0xFFF5c40cc);
 Color kBlackColor = const Color(0xFF01F1449);
@@ -8,3 +9,27 @@ Color kGreenColor = const Color(0xFF0EC3AE);
 Color kRedColor = const Color(0xFFEB70A5);
 Color kBackgroundColor = const Color(0xFFFAFAFA);
 Color kInactiveColor = const Color(0xFFDBD7EC);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: kBlackColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: kWhiteColor,
+);
+
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  color: kGreyColor,
+);
+
+TextStyle redTextStyle = GoogleFonts.poppins(
+  color: kRedColor,
+);
+
+TextStyle greenTextStyle = GoogleFonts.poppins(
+  color: kGreenColor,
+);
+
+TextStyle purpleTextStyle = GoogleFonts.poppins(
+  color: kPrimaryColor,
+);
