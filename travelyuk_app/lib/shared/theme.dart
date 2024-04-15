5da257a0b517683c83c9c9d651da7e83b5849c0b
rizki <rizkiadnanfutuh@gmail.com>
2024-04-16 01:11:04 +0700

    Add Shared Color Variables

---
+++ travelyuk_app/lib/shared/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Color kPrimaryColor = const Color(0xFFF5c40cc);
+Color kBlackColor = const Color(0xFF01F1449);
+Color kWhiteColor = const Color(0xFFFFFFFF);
+Color kGreyColor = const Color(0xFF9698A9);
+Color kGreenColor = const Color(0xFF0EC3AE);
+Color kRedColor = const Color(0xFFEB70A5);
+Color kBackgroundColor = const Color(0xFFFAFAFA);
+Color kInactiveColor = const Color(0xFFDBD7EC);
