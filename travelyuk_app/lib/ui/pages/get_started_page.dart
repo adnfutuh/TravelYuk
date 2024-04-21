a375fa25f0940934ae2045a1b07923c51b428ca2
rizki <rizkiadnanfutuh@gmail.com>
2024-04-22 01:38:37 +0700

    Create Get Started Page

---
+++ travelyuk_app/lib/ui/pages/get_started_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import '../../../shared/theme.dart';
+
+class GetStartedPage extends StatelessWidget {
+  const GetStartedPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
